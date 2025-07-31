eaac949ae40dd5f4574b4b2ccc9481184dcf7d68
ishan <raiishan0@gmail.com>
2025-08-01 01:00:42 +0530

    updated petRepository

--- src/main/java/com/ishan/repository/PetRepository.java
+++ src/main/java/com/ishan/repository/PetRepository.java
@@ -5,5 +5,5 @@ import com.ishan.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PetRepository extends JpaRepository<Pet, Integer> {
-	
+	// all CRUD operations are already provided by JpaRepository
 }
